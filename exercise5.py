9d9e236812b87559f5afa7287f43c2bf7a67ead6
Kazaxthan <carlo.paule.29@gmail.com>
2024-01-24 03:17:37 +0800

    Second commit, here i finished my coding where it will ask the user to input a list of number seperated by space, and the program will identify if the first and last number is the same by telling the user if it is true or false.

--- exercise5.py
+++ exercise5.py
@@ -1,3 +1,27 @@
 #exercise5
 #Exercise 5: Check if the first and last number of a list is the same
 #Write a function to return True if the first and last number of a given list is same. If numbers are different then return False.
+
+def first_and_last_same(list):
+    first_digit = list[0]
+    last_digit = list[-1]
+
+    if first_digit == last_digit:
+        return True
+    else:
+        return False
+
+# Ask the user for input
+user_input = input("Enter a list of numbers separated by spaces: ")
+
+# Convert the user input into a list of integers
+try:
+    user_list = [int(x) for x in user_input.split()]
+except ValueError:
+    print("Invalid input. Please enter a valid list of numbers.")
+    exit()
+
+# result
+result = first_and_last_same(user_list)
+
+print(f"The first and last numbers in the list are the same: {result}","\n Original numbers:", user_input)
